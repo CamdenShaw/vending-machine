f91e648a4321ba2ce2434bc259aee055fd9f2de7
Camden_Shaw <camden.shaw@gmail.com>
2017-11-19 15:43:22 -0800

    vending machine determines value of coins based on size and weight, totals value and prints inventory

--- __test__/vending-machine.spec.js
+++ __test__/vending-machine.spec.js
@@ -1,12 +1,13 @@
-jest.setMock("../src/inventory", require("../__mocks__/inventory-data.mock"))
+jest.setMock("../src/inventory", require("../__mocks__/inventory.mock"))
 
-const vendingMachine = require("../src/vendingMachine")
+const vendingMachine = require("../src/vendingMachine.js")
 
 describe("A virtual machine that dispenses treats", () => {
     beforeEach(() => {
         test = {}
+        test.itemName = "Coffee Crisp"
         test.inventory = {
-            coffeeCrisp: {
+            [test.itemName]: {
                 price: 1.50,
                 stock: 4,
                 desiredStock: 10,
@@ -16,11 +17,27 @@ describe("A virtual machine that dispenses treats", () => {
         test.vendingMachine = new vendingMachine(test.inventory)
     })
     describe("When money is put into the machine", () => {
-        it("Should throw an error if the arguments are not numbers", () => {
-            expect(() => test.vendingMachine.countMoney('4', '5', '6')).toThrow()
+        describe("When a coin is inserted", () => {
+            it("Should return the value based on weight, diameter and thickness", () => {
+                expect(test.vendingMachine.coinAnalysis(28, 7.3, 1.8)).toBe(2.00)
+                expect(test.vendingMachine.coinAnalysis(26.5, 6.27, 1.95)).toBe(1.00)
+                expect(test.vendingMachine.coinAnalysis(23.88, 4.4, 1.58)).toBe(0.25)
+                expect(test.vendingMachine.coinAnalysis(18.03, 1.75, 1.22)).toBe(0.10)
+                expect(test.vendingMachine.coinAnalysis(21.2, 3.95, 1.76)).toBe(0.05)
+            })
         })
-        it("Should display the total of money put into the machine", () => {
-            expect(test.vendingMachine.countMoney(4, 5, 6)).toEqual(15)
+        describe("When value of coins has been returned", () => {
+            it("Should throw an error if the arguments are not numbers", () => {
+                expect(() => test.vendingMachine.countMoney('4', '5', '6')).toThrow()
+            })
+            it("Should display the total of money put into the machine", () => {
+                expect(test.vendingMachine.countMoney(4, 5, 6)).toBe(15)
+            })
+        })
+    })
+    describe("When an inventory count is requested", () => {
+        it("Should return the stock count of all items in the machine", () => {
+            expect(test.vendingMachine.printInventory()).toEqual({"Coffee Crisp": 4})
         })
     })
 })
--- src/vendingMachine.js
+++ src/vendingMachine.js
@@ -1,21 +1,34 @@
-const processedData = require("./inventory")
+const inventory = require("./inventory")
 
 class VendingMachine {
     constructor(data){
-        this.data = processedData(data)
+        this.data = inventory(data)
     }
 
-    countMoney(...count){
+    coinAnalysis(diameterMM, weightG, thicknessMM) {
+        if((diameterMM > 20.89 && diameterMM < 21.27)&&(weightG > 3.94 && weightG < 4.55)&&(thicknessMM > 1.69 && thicknessMM < 1.77)) return 0.05
+        else if ((diameterMM > 18.029 && diameterMM < 18.035)&&(weightG > 1.74 && weightG < 2.34)&&(thicknessMM > 1.16 && thicknessMM < 1.23)) return 0.10
+        else if((diameterMM > 23.61 && diameterMM < 23.89)&&(weightG > 4.39 && weightG < 5.84)&&(thicknessMM > 1.57 && thicknessMM < 1.61)) return 0.25
+        else if((diameterMM > 26.49 && diameterMM < 26.73)&&(weightG > 6.26 && weightG < 7.01)&&(thicknessMM > 1.74 && thicknessMM < 1.96)) return 1.00
+        else if((diameterMM > 27.9 && diameterMM < 28.1)&&(weightG > 6.92 && weightG < 7.31)&&(thicknessMM > 1.74 && thicknessMM < 1.81)) return 2.00
+        else return "reject"
+    }
+
+    countMoney(...count) {
+        let total = 0
         for(let item of count){
-            console.log(item)
-            if(typeof item !== 44) throw new Error("money is typically counted in numbers")
-            total += count
+            if(typeof item !== 'number') throw new Error("money is typically counted in numbers")
+            else total += item
         }
-        return 15
+        return total
     }
     
     printInventory(){
-    
+        let inventoryCount = {}
+        Object.keys(this.data).forEach((item) => {
+            inventoryCount[item] = this.data[item].stock
+        })
+        return inventoryCount
     }
     
     refillInventory(){
@@ -33,10 +46,6 @@ class VendingMachine {
     changeToReturn() {
 
     }
-    countMoney() {
-
-    }
-    
 }
 
 module.exports = VendingMachine
