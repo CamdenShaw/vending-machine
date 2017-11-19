af12f0209fb5988fdb7fc6603981aa67c9295150
Camden_Shaw <camden.shaw@gmail.com>
2017-11-18 23:56:27 -0800

    testing count function within vending machine class

--- __test__/vending-machine.spec.js
+++ __test__/vending-machine.spec.js
@@ -1,27 +1,26 @@
+jest.setMock("../src/inventory", require("../__mocks__/inventory-data.mock"))
+
 const vendingMachine = require("../src/vendingMachine")
-    constructor()
 
-    printInventory(){
-
-    }
-
-    refillInventory(){
-
-    }
-
-    restockChange() {
-
-    }
-
-    giveTreat() {
-
-    }
-
-    changeToReturn() {
-
-    }
-    countMoney() {
-
-    }
-
-describe("A virtual machine that ")
+describe("A virtual machine that dispenses treats", () => {
+    beforeEach(() => {
+        test = {}
+        test.inventory = {
+            coffeeCrisp: {
+                price: 1.50,
+                stock: 4,
+                desiredStock: 10,
+                callCode: 'C5'
+            }
+        }
+        test.vendingMachine = new vendingMachine(test.inventory)
+    })
+    describe("When money is put into the machine", () => {
+        it("Should throw an error if the arguments are not numbers", () => {
+            expect(() => test.vendingMachine.countMoney('4', '5', '6')).toThrow()
+        })
+        it("Should display the total of money put into the machine", () => {
+            expect(test.vendingMachine.countMoney(4, 5, 6)).toEqual(15)
+        })
+    })
+})
--- src/vendingMachine.js
+++ src/vendingMachine.js
@@ -1,5 +1,42 @@
-class VendingMachine {
+const processedData = require("./inventory")
 
+class VendingMachine {
+    constructor(data){
+        this.data = processedData(data)
+    }
+
+    countMoney(...count){
+        for(let item of count){
+            console.log(item)
+            if(typeof item !== 44) throw new Error("money is typically counted in numbers")
+            total += count
+        }
+        return 15
+    }
+    
+    printInventory(){
+    
+    }
+    
+    refillInventory(){
+
+    }
+
+    restockChange() {
+
+    }
+
+    giveTreat() {
+
+    }
+
+    changeToReturn() {
+
+    }
+    countMoney() {
+
+    }
+    
 }
 
-modules.exports = VendingMachine
+module.exports = VendingMachine
